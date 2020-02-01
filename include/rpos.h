b7dccf6cd322a2154f13fdb3dbe46f94e254f28d
NomoreOnDebian <sgtr_t@yahoo.com>
2020-02-01 11:30:20 +0800

    multi-sourcefile structure

---
+++ include/rpos.h
@@ -0,0 +1,90 @@
+#ifndef __RPOS_H__
+#define __RPOS_H__
+
+#ifndef MAX
+#define MAX 100
+#endif
+
+extern int cn;
+
+//data structure block
+//********************
+typedef struct {
+	char name[MAX];
+	long long cell;
+	long long tel;
+	char email[MAX];
+	long long qq;
+	char city[MAX];
+	long long eip;
+}RPvalue;//was named contact
+
+typedef struct RPdata{
+	RPvalue data;
+	struct RPdata *prior;
+	struct RPdata *next;
+}RPdata;//was named cabin
+//********************
+//data structure block end
+
+
+//functions
+//********************
+RPdata *rpContactInit();
+
+RPdata *rpContactGen(RPdata *C);
+
+void rpStringInput(char C[]);
+
+long long rpLLInput();
+
+void rpContactInput(RPdata *newbee);
+
+void rpContinue();
+
+void rpContactMenu(RPdata *C);
+
+void rpUI_1(RPdata *C);
+
+void rpShowName(RPdata *C);
+
+void rpContactCall(RPdata *C);
+
+void rpContactText(RPdata *C);
+
+void rpContactDetail(RPdata *C);
+
+void rpContactDelete(RPdata *C);
+
+int rpShellInput(int s);
+
+void rpContactMenu_Screen();
+
+void rpUI_3_Screen();
+
+void rpUI_Main_Screen();
+
+void rpUI_2_Screen();
+
+void rpContactList(int target,int root,int found,RPdata *C);
+
+void rpUI_2(int i,RPdata *C);
+
+void rpUserManual();
+
+int rpUI_Main(RPdata *C);
+
+void rpFilePurge();
+
+RPdata *rpFileLoad();
+
+void rpFileBurner();
+
+void rpFileNuke();
+
+void rpFileBrowse();
+//********************
+//funcion block end
+
+#endif
+
--- makefile
+++ makefile
@@ -2,8 +2,8 @@ CC = gcc
 all: run OS.out user_manual user_manual_zh main.save
 run:
 	mkdir ../run
-OS.out: src/main.c
-	$(CC) -o ../run/OS.out src/main.c
+OS.out: src/main.c src/rpos.c include/rpos.h
+	$(CC) -o ../run/OS.out src/main.c src/rpos.c
 user_manual: etc/user_manual
 	cp etc/user_manual ../run/user_manual
 user_manual_zh: etc/user_manual_zh
